716a82babe9102751af2f43fa335e1478e0f639c
pililo777 <pililo777@539aa792-33f6-afa4-7e4e-94bd1a4c303e>
2014-02-02 20:56:59 +0000

    ultimas modificaciones

---
+++ CppApplication_1/nbproject/Makefile-Windows.mk
@@ -0,0 +1,107 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++-4.7
+CXX=g++-4.7
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_CONF=Windows
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/run.o \
+	${OBJECTDIR}/grammar.tab.o \
+	${OBJECTDIR}/lex.yy.o \
+	${OBJECTDIR}/err.o \
+	${OBJECTDIR}/main.o
+
+
+# C Compiler Flags
+CFLAGS=-mms-bitfields -I/cygdrive/c/gtk/include/gtk-2.0 -I/cygdrive/c/gtk/lib/gtk-2.0/include -I/cygdrive/c/gtk/include/atk-1.0 -I/cygdrive/c/gtk/include/cairo -I/cygdrive/c/gtk/include/gdk-pixbuf-2.0 -I/cygdrive/c/gtk/include/pango-1.0 -I/cygdrive/c/gtk/include/glib-2.0 -I/cygdrive/c/gtk/lib/glib-2.0/include -I/cygdrive/c/gtk/include -I/cygdrive/c/gtk/include/freetype2 -I/cygdrive/c/gtk/include/libpng14
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cppapplication_1
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cppapplication_1: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.c} -L/cygdrive/c/gtk/lib -lgtk-win32-2.0 -lgdk-win32-2.0 -latk-1.0 -lgio-2.0 -lpangowin32-1.0 -lgdi32 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lpango-1.0 -lcairo -lfontconfig -lfreetype -lpng14 -lz -lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lglib-2.0 -lintl -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cppapplication_1 ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/run.o: run.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.c) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/run.o run.c
+
+${OBJECTDIR}/grammar.tab.o: grammar.tab.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.c) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/grammar.tab.o grammar.tab.c
+
+${OBJECTDIR}/lex.yy.o: lex.yy.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.c) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/lex.yy.o lex.yy.c
+
+${OBJECTDIR}/err.o: err.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.c) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/err.o err.c
+
+${OBJECTDIR}/main.o: main.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.c) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.c
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cppapplication_1
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
--- CppApplication_1/nbproject/Makefile-variables.mk
+++ CppApplication_1/nbproject/Makefile-variables.mk
@@ -6,6 +6,38 @@
 CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=cppapplication_1
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/cppapplication_1
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=cppapplication1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/cppapplication1.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=cppapplication_1
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/cppapplication_1
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=cppapplication1.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/cppapplication1.tar
+# Windows configuration
+CND_PLATFORM_Windows=GNU-Linux-x86
+CND_ARTIFACT_DIR_Windows=dist/Windows/GNU-Linux-x86
+CND_ARTIFACT_NAME_Windows=cppapplication_1
+CND_ARTIFACT_PATH_Windows=dist/Windows/GNU-Linux-x86/cppapplication_1
+CND_PACKAGE_DIR_Windows=dist/Windows/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Windows=cppapplication1.tar
+CND_PACKAGE_PATH_Windows=dist/Windows/GNU-Linux-x86/package/cppapplication1.tar
+# Windows_E configuration
+CND_PLATFORM_Windows_E=GNU-Linux-x86
+CND_ARTIFACT_DIR_Windows_E=dist/Windows_E/GNU-Linux-x86
+CND_ARTIFACT_NAME_Windows_E=cppapplication_1
+CND_ARTIFACT_PATH_Windows_E=dist/Windows_E/GNU-Linux-x86/cppapplication_1
+CND_PACKAGE_DIR_Windows_E=dist/Windows_E/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Windows_E=cppapplication1.tar
+CND_PACKAGE_PATH_Windows_E=dist/Windows_E/GNU-Linux-x86/package/cppapplication1.tar
 #
 # include compiler specific variables
 #
