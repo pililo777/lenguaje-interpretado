95460ccf4c201a4e44364aad9410138f9b748907
Ruben <pililo777@gmail.com>
2017-10-09 02:14:53 +0200

    segunda prueba branch

--- CppApplication_1/bt.c
+++ CppApplication_1/bt.c
@@ -1,5 +1,5 @@
 //usando sublime como editor de texto alternativo a Netbeans
-// version para github  rama errorMem
+// version para github  rama errorMem segunda prueba
 extern int depurar;
 
 #include "stdio.h"
